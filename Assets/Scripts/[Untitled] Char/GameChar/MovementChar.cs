6692199d8427cd716352dea744f1f5098f69ec29
Scott van den Broek <you@example.com>
2021-03-06 16:35:56 +0100

    (Tijdelijk) objecten geplaats om met collision te testen

---
+++ Assets/Scripts/[Untitled] Char/GameChar/MovementChar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementChar : MonoBehaviour
+{
+    
+    //this bool is for the ability to move the char
+    public bool AbleToMove = true;
+    public float SpeedGameChar = 4f;
+
+    // called once every frame
+    void FixedUpdate()
+    {
+        
+        if(AbleToMove == true)
+        {
+            //if W is pressed GameChar goes up
+            if(Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+            }
+
+            //if S is pressed GameChar goes down
+            if(Input.GetKey(KeyCode.S))
+            {
+                transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+            }
+
+            //if D is pressed GameChar goes to the Left
+            if(Input.GetKey(KeyCode.D))
+            {
+                transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+            }
+
+            //if A is pressed GameChar goes to the right
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+            }
+        }
+
+        
+        
+    }
+
+}
